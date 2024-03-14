93567c715336245239c6783c878691e8382211f7
MT-code2023 <mt231201@hotmail.com>
2024-03-14 19:29:37 +0000

    changes

---
+++ test2.py
@@ -0,0 +1,10 @@
+# send the required phrase to the screen.
+print("Hello World!")
+print("Hello World with amendments!")
+
+# takes user input
+hello_world = input("Would you like to say hello to the world? ")
+if hello_world.lower() == "yes":
+    print("Hello World!")
+else:
+    print("Very well, have a nice day.")
