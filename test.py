91a68b90b52c61a542899d3108f564d4c8557ad4
MT-code2023 <mt231201@hotmail.com>
2024-03-19 20:11:54 +0000

    variable to take user input added

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-# send the required phrase to the screen.
+# Send the required phrase to the screen.
 print("Hello World!")
 print("Hello World with amendments!")
+
+# Creates a variable to take user input & print to the screen.
+wonderful_world = input("Please type what you love about the world:" )
+print(wonderful_world)
